c9918da9dc7b6e1850597952e1dd9d5da14578be
johnprif <giannispriftis37@gmail.com>
2022-10-30 02:00:29 +0300

    create HelloFX.java for testing purposes only

--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,17 @@
  * @author Joanis Prifti
  *
  */
-module Java_Paper {
+module Java_Paper 
+{
+	exports View;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires java.sql;
+	requires java.desktop;
+	requires java.xml;
+//	requires com.healthmarketscience.jackcess;
+	
+	opens View to javafx.graphics, javafx.fxml;
 }
