f7ac42a3d67634bb8509e3c622e166276912201a
johnprif <giannispriftis37@gmail.com>
2022-11-01 17:28:00 +0200

    Update title

--- src/View/HelloFX.java
+++ src/View/HelloFX.java
@@ -13,9 +13,12 @@ public class HelloFX extends Application
 	{
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
+//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//        Scene scene = new Scene(new StackPane(l), 640, 480);
+        
+        stage.setTitle("Sven Skyum 1991, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        
+//        stage.setScene(scene);
         stage.show();
     }
 
