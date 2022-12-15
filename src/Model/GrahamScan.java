ba00005e73c175c04b2a2f64fe4779c8cc5679c1
johnprif <56134761+johnprif@users.noreply.github.com>
2022-12-15 13:13:22 +0200

    nothing

--- src/Model/GrahamScan.java
+++ src/Model/GrahamScan.java
@@ -3,12 +3,19 @@
 //https://www.tutorialspoint.com/Graham-Scan-Algorithm
 //https://algorithmtutor.com/Computational-Geometry/Convex-Hull-Algorithms-Graham-Scan/
 //https://blog.devgenius.io/grahams-scan-visually-explained-be54b712e2ba
+//https://www.geeksforgeeks.org/stack-class-in-java/
+//https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/Stack.html
+//https://stackabuse.com/quicksort-in-java/
 
+//copy this implementation
+//https://github.com/dochvam/GrahamScan
+//Sort of arrayList is implemented with merge sort witch is far quicker than o(nlogn)
 package Model;
-
-import java.util.Stack;
 
 public class GrahamScan 
 {
-	Stack<Integer> even = new Stack<>();
+	
+	
 }
+
+
