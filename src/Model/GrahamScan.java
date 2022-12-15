7d65cc2d98102e4bd684ea48bbdc595a787f923c
johnprif <56134761+johnprif@users.noreply.github.com>
2022-12-15 11:40:32 +0200

    Create GrahamScan Class

---
+++ src/Model/GrahamScan.java
@@ -0,0 +1,14 @@
+//https://www.geeksforgeeks.org/convex-hull-using-graham-scan/
+//https://iq.opengenus.org/graham-scan-convex-hull/
+//https://www.tutorialspoint.com/Graham-Scan-Algorithm
+//https://algorithmtutor.com/Computational-Geometry/Convex-Hull-Algorithms-Graham-Scan/
+//https://blog.devgenius.io/grahams-scan-visually-explained-be54b712e2ba
+
+package Model;
+
+import java.util.Stack;
+
+public class GrahamScan 
+{
+	Stack<Integer> even = new Stack<>();
+}
