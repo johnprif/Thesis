698abcec0370698a2d7ae0576ebfeb4083c9b846
johnprif <56134761+johnprif@users.noreply.github.com>
2022-12-16 12:48:49 +0200

    implement getAngle

--- src/Model/Algorithm1.java
+++ src/Model/Algorithm1.java
@@ -1,10 +1,11 @@
 //https://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
-
+//https://www.geeksforgeeks.org/java-math-acos-method-examples/
+//https://www.geeksforgeeks.org/java-sqrt-method-examples/
 package Model;
 
 import java.util.ArrayList;
 import javafx.geometry.Point2D;
-
+import java.lang.Math;
 public class Algorithm1 
 {
 	private ArrayList<Point2D> convexPoints;
@@ -44,6 +45,25 @@ public class Algorithm1
 		{
 			System.out.println("Only 1 point");
 		}
+	}
+	
+	private double getRadius(Point2D p, Point2D q, Point2D r)
+	{
+		if((p!=q) && (q!=r) && (p!=r))
+		{
+			
+		}
+	}
+	
+	private double getAngle(Point2D p, Point2D q, Point2D r)
+	{
+		Point2D pq = new Point2D(p.getX()-q.getX(), p.getY()-q.getY());
+		Point2D qr = new Point2D(q.getX()-r.getX(), q.getY()-r.getY());
+		double pq_qr = pq.getX()*qr.getX()+pq.getY()*qr.getY();
+		double _pq_qr_ = Math.sqrt(pq.getX()*pq.getX()+pq.getY()*pq.getY()) * Math.sqrt(qr.getX()*qr.getX()+qr.getY()*qr.getY());		
+		double angle = Math.acos(pq_qr/_pq_qr_);
+		
+		return angle;
 	}
 	
 	private Point2D findMax()
