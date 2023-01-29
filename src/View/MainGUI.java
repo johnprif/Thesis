8fc2be7c1ed1d39c2934b5be48a7231afe5877b7
johnprif <giannispriftis37@gmail.com>
2023-01-29 23:32:29 +0200

    nothing

--- src/View/MainGUI.java
+++ src/View/MainGUI.java
@@ -10,18 +10,15 @@ import Control.PaperTitleHandler;
 import Control.PutPointsButtonHandler;
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
