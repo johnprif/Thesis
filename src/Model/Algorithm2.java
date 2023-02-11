77d96240ae97a4074bbf30a77cb4f98cad350535
johnprif <giannispriftis37@gmail.com>
2023-02-11 12:41:27 +0200

    Update Algorithm2.java

--- src/Model/Algorithm2.java
+++ src/Model/Algorithm2.java
@@ -1,5 +1,7 @@
 package Model;
 
+import javafx.geometry.Point2D;
+
 public class Algorithm2 
 {
 	private DataBase dataBase;
@@ -12,19 +14,19 @@ public class Algorithm2
 	{
 		System.out.println("I am the second algorithm!");
 		double n = dataBase.getCirclePointsSize();
-		
+		//I have to make (K,E) ArrayLits for each K, E
 		if(n > 2)
 		{
 			do
 			{
 				//find p maximizing radius and angle
-				//q=before(p);
-				//c=centre(q, p, next(p));
+				//q=before(p); --> convexPoints
+				//c=centre(q, p, next(p)); --> public Point2D getCenter(Point2D p, Point2D q, Point2D r)
 				//add c to K;
 				//add (c, u(p)) and (c, u(q)) to E;
-				//u(q)=c;
-				//next(q)=next(p);
-				//before(next(q))=q;
+				//u(q)=c; --> public Point2D getUP(Point2D p, Point2D nextP)
+				//next(q)=next(p); --> convexPoints
+				//before(next(q))=q; --> convexPoints
 				n = n-1;
 			}while(n == 2);
 			//add (u(q), u(next(q))) to E;
