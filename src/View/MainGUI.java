16e9b1e3ee5cdb238ac42bf63a4b06bf9e2f4ff6
johnprif <giannispriftis37@gmail.com>
2023-02-15 11:12:55 +0200

    Update version -> 0.9.5

--- src/View/MainGUI.java
+++ src/View/MainGUI.java
@@ -26,7 +26,7 @@ public class MainGUI extends Application
 {
 	private String javaVersion = System.getProperty("java.version");
     private String javafxVersion = System.getProperty("javafx.version");
-	private String version = "v0.9";
+	private String version = "v0.9.5";
     
 	private Stage stage;
 	private Scene scene;
