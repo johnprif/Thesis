c04011059f9cb1564abc344fc1ddab30da12ef92
johnprif <56134761+johnprif@users.noreply.github.com>
2023-06-03 10:31:55 +0300

    delete unused import

--- src/View/MainGUI.java
+++ src/View/MainGUI.java
@@ -11,7 +11,6 @@ import Control.PutPointsButtonHandler;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
